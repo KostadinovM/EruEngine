7cf74c47232cd397e75395f68788d9a82881664b
KostadinovM <martin.e.kostadinov@gmail.com>
2021-07-15 18:32:22 +0300

    Scripting and script component

--- Engine/Components/Transform.cpp
+++ Engine/Components/Transform.cpp
@@ -11,7 +11,7 @@ utl::vector<math::v3> scales;
 } //anonymous namespace
 
 
-component create_transform(const init_info& info, game_entity::entity entity)
+component create(init_info info, game_entity::entity entity)
 {
 	assert(entity.is_valid());
 	const id::id_type entity_index{ id::index(entity.get_id()) };
@@ -34,7 +34,7 @@ component create_transform(const init_info& info, game_entity::entity entity)
 	return component(transform_id{ (id::id_type)positions.size() - 1 }); //returns new component id
 }
 
-void remove_transform(component c)
+void remove(component c)
 {
 	//all removed components will be reused
 	assert(c.is_valid());
---
+++ Engine/Core/Main.cpp
@@ -0,0 +1,12 @@
+/* This to do to create a game project
+		1) Generate an MSVC solution/project
+		2) Add files that contain the script
+		3) Set include and library directories
+		4) Set force include file (GameEntity.h)
+		5) Set C++ language version and calling convetion (fastcall)
+
+*/
+int main()
+{
+	return 0;
+}
